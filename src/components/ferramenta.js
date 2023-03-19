ec3dc07bf0b5ae1553a51ffa6cf20b67b76f3170
renato-mauro <renato.mauro@gmail.com>
2023-03-19 20:38:17 -0300

    tradução da ferramenta para inglês

--- src/components/ferramenta.js
+++ src/components/ferramenta.js
@@ -243,7 +243,7 @@ function menuListener(inspector, router) {
     return inspector;
 }
 
-function adjustObservableWidth(visRef) {
+function adjustObservableWidth(visRef,router) {
     const library = new Library();
     mainNotebook.redefine("width", library.Generators.observe(notify => {
       let width = null;
@@ -262,10 +262,28 @@ function adjustObservableWidth(visRef) {
       return () => removeEventListener("resize", resized);
     }));
 
+    mainNotebook.redefine("lang",window.location.href.indexOf('/en/')>=0?'en':'pt');
+
     mainNotebook.value("linkParam")
       .then(f=>{linkParam=f})
       .catch(err=>{})
     ;
+
+    function handleRouteChange(url) {
+      mainNotebook.redefine("lang",url.indexOf('/en/')>=0?'en':'pt');
+    }
+
+    function handleRouteChangeStart() {
+      window.dispatchEvent(new CustomEvent("pagehide"));
+    }
+
+    router.events.on("routeChangeStart",handleRouteChangeStart);
+    router.events.on("routeChangeComplete",handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeStart", handleRouteChangeStart)
+      router.events.off("routeChangeComplete", handleRouteChange)
+    };
   }
 
 export function Mapa() {
@@ -276,6 +294,7 @@ export function MapaInternal() {
   const mainWindowRef = useRef();
   const allStylesRef = useRef();
   const stylesRef = useRef();
+  const router = useRouter();
 
   useEffect(() => {
     const runtime = new Runtime();
@@ -283,11 +302,11 @@ export function MapaInternal() {
       if (name === "mainWindow") return new Inspector(mainWindowRef.current);
       if (name === "allStyles") return new Inspector(allStylesRef.current);
       if (name === "styles") return new Inspector(stylesRef.current);
-      return ["clearMainWindow","panel","populate","afterInitialLayout","populateMapa","viewof dorling","viewof colorSelect", "color", "dorlingCircleConf","storageUpdate","scatterConfig","biglayoutToggle","updateScatterConfig","reactToMunSelecionados","xaxis","yaxis","initialLoad"].includes(name);
+      return ["clearMainWindow","panel","populate","afterInitialLayout","populateMapa","viewof dorling","viewof colorSelect", "color", "dorlingCircleConf","storageUpdate","scatterConfig","biglayoutToggle","updateScatterConfig","reactToMunSelecionados","xaxis","yaxis","initialLoad","setOnPageUnload"].includes(name);
     });
     mainNotebook = main;
-    adjustObservableWidth(mainWindowRef);
-    return () => runtime.dispose();
+    let free = adjustObservableWidth(mainWindowRef,router);
+    return () => {runtime.dispose(); free(); };
   }, []);
 
   return (
@@ -338,11 +357,11 @@ function NascidosVivosInternal() {
       if (name === "div_controles") return new Inspector(div_controlesRef.current);
       if (name === "vis") return new Inspector(visRef.current);
       if (name === "style") return new Inspector(styleRef.current);
-      return ["menu_municipios","barra_municipios","gPESO","funcoesGeradoras","gPIG","gTOTAL","checkFiltros","onfirstload","getCurrentConf","cabecalho","width"].includes(name);
+      return ["menu_municipios","barra_municipios","gPESO","funcoesGeradoras","gPIG","gTOTAL","checkFiltros","onfirstload","getCurrentConf","cabecalho","width","setOnPageUnload"].includes(name);
     });
     mainNotebook = main;
-    adjustObservableWidth(visRef);
-    return () => runtime.dispose();
+    let free = adjustObservableWidth(visRef,router);
+    return () => { runtime.dispose(); free(); };
   }, []);
 
   return (
@@ -379,11 +398,11 @@ function CaracteristicasNascimentoInternal() {
       if (name === "vis") return new Inspector(visRef.current);
       if (name === "div_controles") return new Inspector(div_controlesRef.current);
       if (name === "style") return new Inspector(styleRef.current);
-      return ["menu_municipios","barra_municipios","gPESO","funcoesGeradoras","gROBSON","gTOTAL","checkFiltros","onfirstload","getCurrentConf","cabecalho"].includes(name);
+      return ["menu_municipios","barra_municipios","gPESO","funcoesGeradoras","gROBSON","gTOTAL","checkFiltros","onfirstload","getCurrentConf","cabecalho","setOnPageUnload"].includes(name);
     });
     mainNotebook = main;
-    adjustObservableWidth(visRef);
-    return () => runtime.dispose();
+    let free = adjustObservableWidth(visRef,router);
+    return () => { runtime.dispose(); free(); };
   }, []);
 
   return (
@@ -419,11 +438,11 @@ function MortalidadeInfantilInternal() {
       if (name === "vis") return new Inspector(visRef.current);
       if (name === "div_controles") return new Inspector(div_controlesRef.current);
       if (name === "style") return new Inspector(styleRef.current);
-      return ["menu_municipios","barra_municipios","gPESO","funcoesGeradoras","gROBSON","gTOTAL","checkFiltros","onfirstload","getCurrentConf","cabecalho"].includes(name);
+      return ["menu_municipios","barra_municipios","gPESO","funcoesGeradoras","gROBSON","gTOTAL","checkFiltros","onfirstload","getCurrentConf","cabecalho","setOnPageUnload"].includes(name);
     });
     mainNotebook = main;
-    adjustObservableWidth(visRef);
-    return () => runtime.dispose();
+    let free = adjustObservableWidth(visRef,router);
+    return () => { runtime.dispose(); free(); };
   }, []);
 
   return (
@@ -508,7 +527,7 @@ function MenuFerramenta() {
         <svg width="21" height="20" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M20.0463 5.56474L20.3458 7.11382C20.3733 7.25614 20.3316 7.4032 20.2342 7.51162L19.1251 8.72521L18.1935 9.75057L18.0629 11.7556C18.0582 11.8253 18.0377 11.8929 18.0031 11.9537L16.805 14.0484C16.7522 14.1405 16.6691 14.2115 16.57 14.2494L15.3808 14.7036L13.4687 15.5266L13.3741 16.4042C13.3667 16.472 13.3443 16.5374 13.3085 16.5956L12.4628 17.9686C12.4551 17.9813 12.4468 17.9934 12.4378 18.0051L11.9445 18.6564C11.8838 18.7364 11.8007 18.792 11.709 18.8194L11.6808 18.9072C11.6645 18.9578 11.6395 19.0053 11.6069 19.0472L11.0077 19.8217C10.9192 19.9362 10.7838 20 10.644 20C10.5944 20 10.5441 19.992 10.4955 19.9752C10.3093 19.9117 10.1843 19.7371 10.1843 19.5406V19.1902L10.1244 19.1031L9.18958 18.3831L8.74971 18.2315C8.60805 18.1828 8.49903 18.0681 8.45773 17.9243C8.41644 17.7806 8.44765 17.6256 8.54177 17.5092L9.86913 15.8645L9.83151 15.8189C9.76348 15.7366 9.72635 15.6333 9.72635 15.5266V15.1969L9.59284 15.1558C9.43149 15.106 9.31031 14.9722 9.27734 14.8068L9.21219 14.4811L8.51411 14.3193C8.33594 14.2779 8.19924 14.135 8.16594 13.9555L7.9897 13.0049C7.97849 12.9445 7.97945 12.8826 7.99274 12.8225L8.17539 11.9944L8.06109 11.8476C8.02187 11.7971 7.99354 11.7392 7.97801 11.677L7.90646 11.3911H7.45487C7.24116 11.3911 7.05579 11.2441 7.00713 11.0362L6.89876 10.573L6.57557 10.0052L4.79892 9.3759C4.64092 9.32009 4.52599 9.18257 4.49894 9.01754L4.42706 8.58016L3.74595 9.08357C3.66655 9.14226 3.57066 9.1736 3.4727 9.1736C3.45701 9.1736 3.44116 9.17264 3.42564 9.17104L2.22749 9.04791C1.99315 9.02392 1.81483 8.82644 1.81483 8.59105V8.54995L1.10728 8.34575C1.0162 8.31936 0.935358 8.26547 0.875971 8.19143L0.100726 7.22334C0.00340109 7.10163 -0.0257313 6.93917 0.0235708 6.79125L0.42888 5.57671C0.468417 5.45838 0.554219 5.36115 0.666911 5.30758L1.84732 4.74436C1.88286 4.72741 1.92047 4.7151 1.95905 4.70774L2.12857 3.76508L1.86397 3.53129C1.73799 3.41967 1.68292 3.2484 1.72054 3.08449L1.96721 2.0107C2.01155 1.81721 2.17515 1.67441 2.373 1.65602L3.51833 1.55032C3.53242 1.5492 3.54666 1.54856 3.56059 1.54856C3.67824 1.54856 3.79238 1.59397 3.87818 1.67601L4.15015 1.93603H4.71791L4.91576 1.66338L4.55479 1.04149C4.46067 0.879513 4.47476 0.676426 4.59017 0.528507C4.67885 0.41529 4.81332 0.351964 4.95258 0.351964C4.99516 0.351964 5.03806 0.35788 5.08 0.370034L5.83349 0.587035L7.03582 0.0412568C7.09648 0.0135925 7.16132 0 7.22599 0C7.31291 0 7.39967 0.0244684 7.4749 0.0730817C7.60632 0.157356 7.68572 0.303036 7.68572 0.45911L7.68556 1.24171L8.0803 1.74512L9.03836 1.54171L9.18131 1.34774C9.26871 1.22924 9.40669 1.16096 9.55124 1.16096C9.57541 1.16096 9.59958 1.16288 9.62375 1.16672L10.5387 1.31255L11.1702 0.3032C11.2544 0.168717 11.4019 0.0873206 11.5602 0.0873206H11.5686C11.7302 0.0903581 11.8782 0.177989 11.9588 0.317753L12.8398 1.84906C12.9082 1.96803 12.9198 2.11116 12.8717 2.23924L12.713 2.66172C12.6722 2.77014 12.5918 2.85937 12.4881 2.91118L12.1033 3.10356C12.2282 3.16064 12.3252 3.27258 12.3594 3.41379L12.3868 3.52652L12.5814 3.5027L13.523 2.80115C13.604 2.74086 13.7005 2.71016 13.7979 2.71016C13.8725 2.71016 13.9472 2.72839 14.0154 2.76501L15.1808 3.39057L15.6409 3.59478C15.6585 3.60262 15.6757 3.61157 15.6922 3.62165L16.252 3.96018H17.41C17.5205 3.96018 17.6269 3.99984 17.7105 4.0718L18.8937 5.09204L19.6575 5.19679C19.8526 5.22365 20.0088 5.37141 20.0463 5.56474Z" fill="white"/>
         </svg>
-        Escolha municípios
+        {(router.query.locale == "pt" ? "Escolha municípios" : "Select Municipalities")}
       </div>
   
       <div className="bt bt-trilhas" onClick={trilhaClick}>
@@ -516,7 +535,7 @@ function MenuFerramenta() {
           <path d="M1.35413 0V16C1.35413 17.1046 2.24956 18 3.35413 18H19.3541"/>
           <path d="M4.92517 13.2853L6.51341 9.2739L8.2529 11.7425L10.4462 9.47962L13.0932 9.53104L16.0428 5.57104L19.068 6.08533" strokeWidth="2"/>
         </svg>
-        Explore os dados
+        {(router.query.locale == "pt" ? "Explore os dados" : "Explore Data")}
       </div>
     
     <div className='spacer'/>
